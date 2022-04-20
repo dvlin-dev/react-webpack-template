00dd60176fc3d2853e46d2652da3a416448e8689
zbl <59828992+zbl666@users.noreply.github.com>
2022-04-20 22:09:25 +0800

    chore: slimming postcss.config

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-/* eslint-disable global-require */
 module.exports = {
   plugins: [
     require('postcss-import'), // css导入
     require('postcss-preset-env'), // 浏览器兼容
-    // require('postcss-pxtorem')({// px 转换rem
-    //   rootValue: 75,// 配置75px为1rem
-    //   unitPrecision: 5,
-    //   minPixelValue: 2, // 设置要替换的最小像素值
-    //   propWhiteList: ['*'], // Enables converting of all properties – default is just font sizes.
-    //   selectorBlackList: ['.ig-'], // 忽略的选择器   .ig-  表示 .ig- 开头的都不会转换
-    // }),
   ],
 };
